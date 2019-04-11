a066e3fce8ad15945135d7ab7fec442befb42f82
Antonio Leiva <antonioleivag@gmail.com>
2019-04-11 12:30:06 +0200

    Configure Retrofit and test it with a first request

--- app/src/main/java/com/antonioleiva/mymovies/MainActivity.kt
+++ app/src/main/java/com/antonioleiva/mymovies/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.antonioleiva.mymovies
 
-import androidx.appcompat.app.AppCompatActivity
-
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch(Dispatchers.Main) {
+            val movies = MovieDb.service.listPopularMoviesAsync(getString(R.string.api_key))
+            toast(movies.results.size.toString())
+        }
     }
 }
---
+++ app/src/main/java/com/antonioleiva/mymovies/MovieDbResult.kt
@@ -0,0 +1,27 @@
+package com.antonioleiva.mymovies
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieDbResult(
+    val page: Int,
+    val results: List<Movie>,
+    @SerializedName("total_pages") val totalPages: Int,
+    @SerializedName("total_results") val totalResults: Int
+)
+
+data class Movie(
+    val adult: Boolean,
+    @SerializedName("backdrop_path") val backdropPath: String?,
+    @SerializedName("genre_ids") val genreIds: List<Int>,
+    val id: Int,
+    @SerializedName("original_language") val originalLanguage: String,
+    @SerializedName("original_title") val originalTitle: String,
+    val overview: String,
+    val popularity: Double,
+    @SerializedName("poster_path") val posterPath: String,
+    @SerializedName("release_date") val releaseDate: String,
+    val title: String,
+    val video: Boolean,
+    @SerializedName("vote_average") val voteAverage: Double,
+    @SerializedName("vote_count") val voteCount: Int
+)
