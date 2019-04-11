1eabebb30e00138aea2f31f593ba32eff7816cea
Antonio Leiva <antonioleivag@gmail.com>
2019-04-11 13:44:33 +0200

    Show the title results in an adapter

--- app/src/main/java/com/antonioleiva/mymovies/MainActivity.kt
+++ app/src/main/java/com/antonioleiva/mymovies/MainActivity.kt
@@ -2,19 +2,25 @@ package com.antonioleiva.mymovies
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.antonioleiva.mymovies.databinding.ActivityMainBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
+    private val adapter = MoviesAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.recycler.adapter = adapter
 
         GlobalScope.launch(Dispatchers.Main) {
             val movies = MovieDb.service.listPopularMoviesAsync(getString(R.string.api_key))
-            toast(movies.results.size.toString())
+            adapter.movies = movies.results
         }
     }
 }
