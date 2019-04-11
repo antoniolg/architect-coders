12d71f811d32c4d2161d21cf7eaa649232ecf5f5
Antonio Leiva <antonioleivag@gmail.com>
2019-04-11 12:30:06 +0200

    Configure Retrofit and test it with a first request

---
+++ app/src/main/java/com/antonioleiva/mymovies/Extensions.kt
@@ -0,0 +1,8 @@
+package com.antonioleiva.mymovies
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.toast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
