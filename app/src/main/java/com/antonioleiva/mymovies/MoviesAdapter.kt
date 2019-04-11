54b5cc35013b32fc683a3d96daceb4f69e7aae19
Antonio Leiva <antonioleivag@gmail.com>
2019-04-11 13:44:33 +0200

    Show the title results in an adapter

---
+++ app/src/main/java/com/antonioleiva/mymovies/MoviesAdapter.kt
@@ -0,0 +1,43 @@
+package com.antonioleiva.mymovies
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.antonioleiva.mymovies.databinding.ViewMovieBinding
+import kotlin.properties.Delegates
+
+class MoviesAdapter : RecyclerView.Adapter<MoviesAdapter.ViewHolder>() {
+
+    var movies: List<Movie> by Delegates.observable(emptyList()) { _, old, new ->
+        DiffUtil.calculateDiff(object : DiffUtil.Callback() {
+            override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+                old[oldItemPosition].id == new[newItemPosition].id
+
+            override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+                old[oldItemPosition] == new[newItemPosition]
+
+            override fun getOldListSize(): Int = old.size
+
+            override fun getNewListSize(): Int = new.size
+        }).dispatchUpdatesTo(this)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = parent.inflate(R.layout.view_movie, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = movies.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(movies[position])
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val binding = ViewMovieBinding.bind(view)
+        fun bind(movie: Movie) {
+            binding.movieTitle.text = movie.title
+        }
+    }
+}
