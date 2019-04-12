dae9a3a3a335cf88d21fd3cfd4fd6d717f354781
Antonio Leiva <antonioleivag@gmail.com>
2019-04-12 11:26:37 +0200

    Find region and use it in the request

--- app/src/main/java/com/antonioleiva/mymovies/model/TheMovieDbService.kt
+++ app/src/main/java/com/antonioleiva/mymovies/model/TheMovieDbService.kt
@@ -5,5 +5,8 @@ import retrofit2.http.Query
 
 interface TheMovieDbService {
     @GET("discover/movie?sort_by=popularity.desc")
-    suspend fun listPopularMoviesAsync(@Query("api_key") apiKey: String): MovieDbResult
+    suspend fun listPopularMoviesAsync(
+        @Query("api_key") apiKey: String,
+        @Query("region") region: String
+    ): MovieDbResult
 }
--- app/src/main/java/com/antonioleiva/mymovies/ui/MainActivity.kt
+++ app/src/main/java/com/antonioleiva/mymovies/ui/MainActivity.kt
@@ -1,13 +1,27 @@
 package com.antonioleiva.mymovies.ui
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
 import com.antonioleiva.mymovies.R
 import com.antonioleiva.mymovies.model.MovieDb
 import com.antonioleiva.mymovies.ui.common.CoroutineScopeActivity
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.single.BasePermissionListener
+import kotlin.coroutines.resume
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
 
 class MainActivity : CoroutineScopeActivity() {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     private val adapter = MoviesAdapter()
 
@@ -15,12 +29,62 @@ class MainActivity : CoroutineScopeActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recycler.adapter = adapter
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         launch {
+            val location = getLocation()
             val movies = MovieDb.service
-                .listPopularMoviesAsync(getString(R.string.api_key))
+                .listPopularMoviesAsync(
+                    getString(R.string.api_key),
+                    getRegionFromLocation(location)
+                )
+
             adapter.movies = movies.results
         }
+
+        recycler.adapter = adapter
+    }
+
+    private suspend fun getLocation(): Location? {
+        val success = requestCoarseLocationPermission()
+        return if (success) findLastLocation() else null
+    }
+
+    @SuppressLint("MissingPermission")
+    private suspend fun findLastLocation(): Location? =
+        suspendCancellableCoroutine { continuation ->
+            fusedLocationClient.lastLocation
+                .addOnCompleteListener {
+                    continuation.resume(it.result)
+                }
+        }
+
+    private suspend fun requestCoarseLocationPermission(): Boolean =
+        suspendCancellableCoroutine { continuation ->
+            Dexter
+                .withActivity(this@MainActivity)
+                .withPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                .withListener(object : BasePermissionListener() {
+                    override fun onPermissionGranted(response: PermissionGrantedResponse?) {
+                        continuation.resume(true)
+                    }
+
+                    override fun onPermissionDenied(response: PermissionDeniedResponse?) {
+                        continuation.resume(false)
+                    }
+                }
+                ).check()
+        }
+
+    private fun getRegionFromLocation(location: Location?): String {
+        val geocoder = Geocoder(this@MainActivity)
+        val fromLocation = location?.let {
+            geocoder.getFromLocation(
+                location.latitude,
+                location.longitude,
+                1
+            )
+        }
+        return fromLocation?.firstOrNull()?.countryCode ?: "US"
     }
 }
