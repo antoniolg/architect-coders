67fc7a80e5061c0a8d1146d02dfa5db5dd21d577
Antonio Leiva <antonioleivag@gmail.com>
2019-04-15 17:17:41 +0200

    Remove unused toast extension

--- app/src/main/java/com/antonioleiva/mymovies/ui/Extensions.kt
+++ app/src/main/java/com/antonioleiva/mymovies/ui/Extensions.kt
@@ -7,16 +7,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import kotlin.properties.Delegates
-
-fun Context.toast(message: String) {
-    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-}
 
 fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = true): View =
     LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
