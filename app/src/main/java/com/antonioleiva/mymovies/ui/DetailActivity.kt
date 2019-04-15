e0b1c7464ff8b90f5bbf1289bf55f446f9d8b19f
Antonio Leiva <antonioleivag@gmail.com>
2019-04-15 17:27:50 +0200

    Extract detailed info from DetailActivity to a custom View

--- app/src/main/java/com/antonioleiva/mymovies/ui/DetailActivity.kt
+++ app/src/main/java/com/antonioleiva/mymovies/ui/DetailActivity.kt
@@ -3,8 +3,6 @@ package com.antonioleiva.mymovies.ui
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.text.bold
-import androidx.core.text.buildSpannedString
 import com.antonioleiva.mymovies.databinding.ActivityDetailBinding
 import com.antonioleiva.mymovies.model.Movie
 
@@ -17,34 +15,18 @@ class DetailActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityDetailBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        intent.getParcelableExtra<Movie>(MOVIE)?.run {
-            binding.movieDetailToolbar.title = title
+        ActivityDetailBinding.inflate(layoutInflater).run {
+            setContentView(root)
 
-            val background = backdropPath ?: posterPath
-            binding.movieDetailImage.loadUrl("https://image.tmdb.org/t/p/w780$background")
+            val movie = intent.getParcelableExtra<Movie>(MOVIE) ?: throw IllegalStateException()
 
-            binding.movieDetailSummary.text = overview
+            movieDetailToolbar.title = movie.title
 
-            binding.movieDetailInfo.text = buildSpannedString {
-
-                bold { append("Original language: ") }
-                appendLine(originalLanguage)
-
-                bold { append("Original title: ") }
-                appendLine(originalTitle)
-
-                bold { append("Release date: ") }
-                appendLine(releaseDate)
-
-                bold { append("Popularity: ") }
-                appendLine(popularity.toString())
-
-                bold { append("Vote Average: ") }
-                append(voteAverage.toString())
-            }
+            val background = movie.backdropPath ?: movie.posterPath
+            movieDetailImage.loadUrl("https://image.tmdb.org/t/p/w780$background")
+            movieDetailSummary.text = movie.overview
+            movieDetailInfo.setMovie(movie)
         }
     }
 }
---
+++ app/src/main/java/com/antonioleiva/mymovies/ui/MovieDetailInfoView.kt
@@ -0,0 +1,36 @@
+package com.antonioleiva.mymovies.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.text.bold
+import androidx.core.text.buildSpannedString
+import com.antonioleiva.mymovies.model.Movie
+
+class MovieDetailInfoView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : AppCompatTextView(context, attrs, defStyleAttr) {
+
+    fun setMovie(movie: Movie) = with(movie) {
+        text = buildSpannedString {
+
+            bold { append("Original language: ") }
+            appendLine(originalLanguage)
+
+            bold { append("Original title: ") }
+            appendLine(originalTitle)
+
+            bold { append("Release date: ") }
+            appendLine(releaseDate)
+
+            bold { append("Popularity: ") }
+            appendLine(popularity.toString())
+
+            bold { append("Vote Average: ") }
+            append(voteAverage.toString())
+        }
+    }
+}
