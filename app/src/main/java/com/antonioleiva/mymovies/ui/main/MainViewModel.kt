5df2db2043c4c9a3776f371488691c8aee54c0e1
Antonio Leiva <antonioleivag@gmail.com>
2019-04-23 13:09:13 +0200

    Migrate to Arch MVVM

---
+++ app/src/main/java/com/antonioleiva/mymovies/ui/main/MainViewModel.kt
@@ -0,0 +1,54 @@
+package com.antonioleiva.mymovies.ui.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.antonioleiva.mymovies.model.Movie
+import com.antonioleiva.mymovies.model.MoviesRepository
+import com.antonioleiva.mymovies.ui.common.Scope
+import kotlinx.coroutines.launch
+
+class MainViewModel(private val moviesRepository: MoviesRepository) : ViewModel(),
+    Scope by Scope.Impl() {
+
+    private val _model = MutableLiveData<UiModel>()
+    val model: LiveData<UiModel>
+        get() {
+            if (_model.value == null) refresh()
+            return _model
+        }
+
+    sealed class UiModel {
+        object Loading : UiModel()
+        class Content(val movies: List<Movie>) : UiModel()
+        class Navigation(val movie: Movie) : UiModel()
+    }
+
+    init {
+        initScope()
+    }
+
+    private fun refresh() {
+        launch {
+            _model.value = UiModel.Loading
+            _model.value = UiModel.Content(moviesRepository.findPopularMovies().results)
+        }
+    }
+
+    fun onMovieClicked(movie: Movie) {
+        _model.value = UiModel.Navigation(movie)
+    }
+
+    override fun onCleared() {
+        destroyScope()
+        super.onCleared()
+    }
+}
+
+@Suppress("UNCHECKED_CAST")
+class MainViewModelFactory(private val moviesRepository: MoviesRepository) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T =
+        MainViewModel(moviesRepository) as T
+}
