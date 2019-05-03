c82ec74cf800b1988fcf92ea67923a9485dafce0
Antonio Leiva <antonioleivag@gmail.com>
2019-05-03 09:25:21 +0200

    Create DB using Room

---
+++ app/src/main/java/com/antonioleiva/mymovies/model/database/MovieDao.kt
@@ -0,0 +1,22 @@
+package com.antonioleiva.mymovies.model.database
+
+import androidx.room.*
+
+@Dao
+interface MovieDao {
+
+    @Query("SELECT * FROM Movie")
+    fun getAll(): List<Movie>
+
+    @Query("SELECT * FROM Movie WHERE id = :id")
+    fun findById(id: Int): Movie
+
+    @Query("SELECT COUNT(id) FROM Movie")
+    fun movieCount(): Int
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertMovies(movies: List<Movie>)
+
+    @Update
+    fun updateMovie(movie: Movie)
+}
