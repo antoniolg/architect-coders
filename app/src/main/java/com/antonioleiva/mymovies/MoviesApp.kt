2e8695231b44a12013310336838ca46b9163b34f
Antonio Leiva <antonioleivag@gmail.com>
2019-05-03 09:25:21 +0200

    Create DB using Room

---
+++ app/src/main/java/com/antonioleiva/mymovies/MoviesApp.kt
@@ -0,0 +1,20 @@
+package com.antonioleiva.mymovies
+
+import android.app.Application
+import androidx.room.Room
+import com.antonioleiva.mymovies.model.database.MovieDatabase
+
+class MoviesApp : Application() {
+
+    lateinit var db: MovieDatabase
+        private set
+
+    override fun onCreate() {
+        super.onCreate()
+
+        db = Room.databaseBuilder(
+            this,
+            MovieDatabase::class.java, "movie-db"
+        ).build()
+    }
+}
---
+++ app/src/main/java/com/antonioleiva/mymovies/model/database/Movie.kt
@@ -0,0 +1,19 @@
+package com.antonioleiva.mymovies.model.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Movie(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    val title: String,
+    val overview: String,
+    val releaseDate: String,
+    val posterPath: String,
+    val backdropPath: String,
+    val originalLanguage: String,
+    val originalTitle: String,
+    val popularity: Double,
+    val voteAverage: Double,
+    val favorite: Boolean
+)
