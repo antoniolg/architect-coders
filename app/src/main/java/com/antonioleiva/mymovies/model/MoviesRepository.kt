e864d1021dede7b6548f0d8816c86ed8c6bfa02f
Antonio Leiva <antonioleivag@gmail.com>
2019-05-07 09:12:42 +0200

    Stop using activity in repository
    With MVVM, we can't rely on the activity object to do anything, as it can change while the VM survives.

--- app/src/main/java/com/antonioleiva/mymovies/model/MoviesRepository.kt
+++ app/src/main/java/com/antonioleiva/mymovies/model/MoviesRepository.kt
@@ -1,12 +1,12 @@
 package com.antonioleiva.mymovies.model
 
-import android.app.Activity
+import android.app.Application
 import com.antonioleiva.mymovies.R
 
-class MoviesRepository(activity: Activity) {
+class MoviesRepository(application: Application) {
 
-    private val apiKey = activity.getString(R.string.api_key)
-    private val regionRepository = RegionRepository(activity)
+    private val apiKey = application.getString(R.string.api_key)
+    private val regionRepository = RegionRepository(application)
 
     suspend fun findPopularMovies() =
         MovieDb.service
