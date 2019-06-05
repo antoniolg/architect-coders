ae60092c66a251075a6a098fb64afbd3782a4262
Antonio Leiva <antonioleivag@gmail.com>
2019-06-05 12:41:25 +0200

    Make use of Koin for dependency injection

--- app/src/main/java/com/antonioleiva/mymovies/data/database/MovieDatabase.kt
+++ app/src/main/java/com/antonioleiva/mymovies/data/database/MovieDatabase.kt
@@ -1,10 +1,20 @@
 package com.antonioleiva.mymovies.data.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Movie::class], version = 1)
 abstract class MovieDatabase : RoomDatabase() {
+
+    companion object {
+        fun build(context: Context) = Room.databaseBuilder(
+            context,
+            MovieDatabase::class.java,
+            "movie-db"
+        ).build()
+    }
 
     abstract fun movieDao(): MovieDao
 }
--- app/src/main/java/com/antonioleiva/mymovies/ui/main/MainActivity.kt
+++ app/src/main/java/com/antonioleiva/mymovies/ui/main/MainActivity.kt
@@ -3,33 +3,28 @@ package com.antonioleiva.mymovies.ui.main
 import android.Manifest.permission.ACCESS_COARSE_LOCATION
 import android.os.Bundle
 import android.view.View
-import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import com.antonioleiva.mymovies.R
 import com.antonioleiva.mymovies.databinding.ActivityMainBinding
 import com.antonioleiva.mymovies.ui.common.PermissionRequester
-import com.antonioleiva.mymovies.ui.common.app
-import com.antonioleiva.mymovies.ui.common.getViewModel
 import com.antonioleiva.mymovies.ui.common.startActivity
 import com.antonioleiva.mymovies.ui.detail.DetailActivity
 import com.antonioleiva.mymovies.ui.main.MainViewModel.UiModel
+import org.koin.androidx.scope.ScopeActivity
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ScopeActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: MoviesAdapter
     private val coarsePermissionRequester =
         PermissionRequester(this, ACCESS_COARSE_LOCATION)
 
-    private lateinit var component: MainActivityComponent
-    private val viewModel: MainViewModel by lazy { getViewModel { component.mainViewModel } }
+    private val viewModel: MainViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        component = app.component.plus(MainActivityModule())
 
         adapter = MoviesAdapter(viewModel::onMovieClicked)
         binding.recycler.adapter = adapter
