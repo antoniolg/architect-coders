c64f41d44541c66086ec3431bf4286b3da605efa
Antonio Leiva <antonioleivag@gmail.com>
2019-06-05 10:59:21 +0200

    Use of subcomponents for activities

--- app/src/main/java/com/antonioleiva/mymovies/di/MyMoviesComponent.kt
+++ app/src/main/java/com/antonioleiva/mymovies/di/MyMoviesComponent.kt
@@ -1,18 +1,20 @@
 package com.antonioleiva.mymovies.di
 
 import android.app.Application
-import com.antonioleiva.mymovies.ui.detail.DetailViewModel
-import com.antonioleiva.mymovies.ui.main.MainViewModel
+import com.antonioleiva.mymovies.ui.detail.DetailActivityComponent
+import com.antonioleiva.mymovies.ui.detail.DetailActivityModule
+import com.antonioleiva.mymovies.ui.main.MainActivityComponent
+import com.antonioleiva.mymovies.ui.main.MainActivityModule
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [AppModule::class, DataModule::class, UseCaseModule::class, ViewModelsModule::class])
+@Component(modules = [AppModule::class, DataModule::class])
 interface MyMoviesComponent {
 
-    val mainViewModel: MainViewModel
-    val detaiViewModel: DetailViewModel
+    fun plus(module: MainActivityModule): MainActivityComponent
+    fun plus(module: DetailActivityModule) : DetailActivityComponent
 
     @Component.Factory
     interface Factory {
