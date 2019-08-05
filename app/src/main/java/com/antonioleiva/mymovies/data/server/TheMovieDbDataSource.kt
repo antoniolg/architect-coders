93c329882a0301e8d2cd575d22cba211dd03105e
Antonio Leiva <antonioleivag@gmail.com>
2019-08-05 10:57:51 +0200

    Extract server url to be able to modify it on tests

--- app/src/main/java/com/antonioleiva/mymovies/data/server/TheMovieDbDataSource.kt
+++ app/src/main/java/com/antonioleiva/mymovies/data/server/TheMovieDbDataSource.kt
@@ -4,10 +4,10 @@ import com.antonioleiva.data.source.RemoteDataSource
 import com.antonioleiva.domain.Movie
 import com.antonioleiva.mymovies.data.toDomainMovie
 
-class TheMovieDbDataSource : RemoteDataSource {
+class TheMovieDbDataSource(private val theMovieDb: TheMovieDb) : RemoteDataSource {
 
     override suspend fun getPopularMovies(apiKey: String, region: String): List<Movie> =
-        TheMovieDb.service
+        theMovieDb.service
             .listPopularMoviesAsync(apiKey, region)
             .results
             .map { it.toDomainMovie() }
